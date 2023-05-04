5eedc1a7061f91283635843ea7270fe9c0ff191a
Ysabel Fernandez <fdzysabel@icloud.com>
2023-05-03 21:09:13 -0400

    finalized code

--- script.js
+++ script.js
@@ -1,91 +1,54 @@
 // Assignment code here
-
-
+var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "x"]
+var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "/"]
+var options = []
 
 // Get references to the #generate element
-
-
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var finalPass = ""
+  var characterSelection = prompt("Length of password characters: ");
+  if (characterSelection < 8) {
+    prompt("Minimum character length is 8. Please try again.");
+    generatePassword()
+  } else if (characterSelection > 128) {
+    prompt("Maxiumum character length is 128. Please try again. ");
+    generatePassword()
+  } else {
+    alert(("Great! Your password will have this amount of characters: " + characterSelection))
+  }
 
-function generatePassword () {
-    var characterSelection = window.prompt("Length of password characters: ");
-if (characterSelection < 8) {
-  window.prompt ("Minimum character length is 8. Please try again.");
-} else if  (characterSelection > 128) {
-  window.prompt ("Maxiumum character length is 128. Please try again. ");
-} else if  (characterSelection > 128) {
-  window.prompt ("Maxiumum character length is 128. Please try again. ");
-} else { 
-  window.prompt (("Great! Your password will have this amount of characters:"), characterSelection)
+  var lowercaseAnswer = confirm("Include lowercase?");
+  if (lowercaseAnswer) {
+    alert("Noted! Your password will include lowercase letters.");
+    options = options.concat(lowerArr)
+  }
+
+  var uppercaseAnswer = confirm("Include uppercase?");
+  if (uppercaseAnswer) {
+    alert("Noted! Your password will include uppercase letters.");
+    options = options.concat(upperArr)
+  }
+
+  var numericAnswer = confirm("Include numbers?");
+  if (numericAnswer) {
+    alert("Noted! Your password will include numbers.");
+    options = options.concat(numArr)
+  }
+
+  var specialAnswer = confirm("Include special character?");
+  if (specialAnswer) {
+    alert("Noted! Your password will special characters.");
+    options = options.concat(specialArr)
+  }
+  console.log(options)
+  for (var i = 0; i < characterSelection; i++) {
+    finalPass = finalPass + options[Math.floor(Math.random() * options.length)]
+  }
+  return finalPass
 }
-
-  var lowercaseAnswer = window.prompt("Include lowercase: yes or no"); 
-if (lowercaseAnswer === "yes") {
-  alert ("Noted! Your password will include lowercase letters."); 
-} else if (lowercaseAnswer === "no") { 
-  alert ("Noted! Your password will NOT include lowercase letters.");
-} else {
-  window.prompt ("Please input 'yes' or 'no' to continue.")
-  return (lowercaseAnswer)
-}
-
-  var uppercaseAnswer = window.prompt("Include uppercase: yes or no"); 
-if (uppercaseAnswer === "yes") {
-  alert ("Noted! Your password will include uppercase letters."); 
-} else if (uppercaseAnswer === "no") { 
-  alert ("Noted! Your password will NOT include upperrcase letters.");
-} else {
-  window.prompt ("Please input yes or no to continue.")
-  return (uppercaseAnswer)
-}
-
-  var numericAnswer = window.prompt("Include numbers: yes or no"); 
-if (numericAnswer === "yes") {
-  alert ("Noted! Your password will include numbers."); 
-} else if (numericAnswer === "no") { 
-  alert ("Noted! Your password will NOT include numbers.");
-} else {
-  window.prompt ("Please input yes or no to continue.")
-  return (numericAnswer)
-}
-
-  var specialAnswer = window.prompt ("Include special character: yes or no"); 
-if (specialAnswer === "yes") {
-  alert ("Noted! Your password will special characters."); 
-} else if (specialAnswer === "no") { 
-  alert ("Noted! Your password will NOT include special characters.");
-} else {
-  window.prompt ("Please input yes or no to continue.")
-  return (specialAnswer)
-}
-}
-// validate input 
-
-
-/* declare password  
-var declarePassword= function(generatePassword) {
-if (specialAnswer, numericAnswer, uppercaseAnswer, lowercaseAnswer === "yes") 
-return 
-}*/
-
-/*
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*(){}[]=<>/.,'"
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-*/
 
 // Write password to the #password input
 function writePassword() {
@@ -94,8 +57,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
